a0e14597950231d21025ccbcf3eb82739fbc418d
orache arthur <arthur.orache@gmail.com>
2020-04-01 16:51:30 +0300

    Update routing

--- src/components/main.js
+++ src/components/main.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { Route, Switch } from "react-router-dom";
-import ShoppingLists from "./patients";
+import Patients from "./patients";
 import Login from "./login.js";
 import Register from "./register.js";
 
 const Main = () => (
   <main>
     <Switch>
-      <Route exact path="/patients" component={ShoppingLists} />
+      <Route exact path="/patients" component={Patients} />
       <Route exact path="/register" component={Register} />
       <Route path="/" component={Login} />
     </Switch>
--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const instance = axios.create({
-  baseURL: "http://127.0.0.1:5000//api/v1"
+  baseURL: "http://127.0.0.1:5000/api/v1"
 });
 
 instance.interceptors.request.use(config => {
